ff82313567883c95cfb86f60589a1a8bc081760b
Ben Smith <ben@hellosmithy.com>
2024-11-21 23:29:33 +0900

    naive navigate to existing room

--- lib/planning_poker_web/live/room_live/index.ex
+++ lib/planning_poker_web/live/room_live/index.ex
@@ -9,7 +9,25 @@ defmodule PlanningPokerWeb.RoomLive.Index do
   @impl true
   def render(assigns) do
     ~H"""
-    <h1>Planning Poker</h1>
+    <h2>Distributed scrum planning poker for estimating agile projects.</h2>
+    <p>
+      First person to create the room is the moderator. Share the url or room number with other team members to join the room.
+    </p>
+
+    <div class="flex flex-col items-center space-y-4">
+      <form phx-submit="join_room" class="space-y-4">
+        <div>
+          <label for="room_number">Enter room number</label>
+          <input id="room_number" name="room_number" placeholder="room no" />
+        </div>
+        <button type="submit">Join Room</button>
+      </form>
+    </div>
     """
+  end
+
+  @impl true
+  def handle_event("join_room", %{"room_number" => room_number}, socket) do
+    {:noreply, push_navigate(socket, to: ~p"/#{room_number}")}
   end
 end
