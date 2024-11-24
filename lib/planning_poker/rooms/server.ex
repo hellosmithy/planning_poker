0cae09949e24ea6c7158eecb90dc11efab78e7d2
Ben Smith <ben@hellosmithy.com>
2024-11-24 21:45:13 +0900

    terminate inactive rooms after 30mins

--- lib/planning_poker/rooms/server.ex
+++ lib/planning_poker/rooms/server.ex
@@ -3,12 +3,23 @@ defmodule PlanningPoker.Rooms.Server do
 
   use GenServer
 
+  @idle_timeout :timer.minutes(30)
+
   def start_link(room_id) do
     GenServer.start_link(__MODULE__, room_id, name: via_tuple(room_id))
   end
 
   defp via_tuple(room_id) do
     {:via, Registry, {PlanningPoker.Rooms.Registry, room_id}}
+  end
+
+  def child_spec(room_id) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [room_id]},
+      restart: :transient,
+      type: :worker
+    }
   end
 
   def set_mode(room_server, mode) do
@@ -23,17 +34,23 @@ defmodule PlanningPoker.Rooms.Server do
   def init(room_id) do
     IO.puts("Starting Room.Server: \"#{room_id}\"")
     Registry.register(PlanningPoker.Rooms.Registry, room_id, :ok)
-    {:ok, Room.new(room_id)}
+    {:ok, Room.new(room_id), @idle_timeout}
   end
 
   @impl GenServer
   def handle_call(:get_mode, _, room) do
-    {:reply, room.mode, room}
+    {:reply, room.mode, room, @idle_timeout}
   end
 
   @impl GenServer
   def handle_cast({:set_mode, mode}, room) do
     new_room = Room.set_mode(room, mode)
-    {:noreply, new_room}
+    {:noreply, new_room, @idle_timeout}
+  end
+
+  @impl true
+  def handle_info(:timeout, state) do
+    IO.puts("Terminating Room.Server for room #{state.id} due to inactivity.")
+    {:stop, :normal, state}
   end
 end
